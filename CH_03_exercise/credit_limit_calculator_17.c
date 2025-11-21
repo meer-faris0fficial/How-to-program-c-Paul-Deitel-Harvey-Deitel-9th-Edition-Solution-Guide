a8ef019d1e05ca1b6e785c3e8c3bfadcdfbf52e4
Meer Faris <mthofficial111@gmail.com>
2025-11-21 21:04:13 +0500

    question no 17 solution.c

---
+++ CH_03_exercise/credit_limit_calculator_17.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main()
+{
+    float accountNumber = 0.0;
+    float beginningBalance = 0.0;
+
+    float totalCharges = 0;
+    float totalCredits = 0;
+    int creditLimit = 0;
+
+    float balance = 0;
+    float limitResult = 0;
+
+    while (accountNumber != -1)
+    {
+        printf("Enter the account number (-1 to end): ");
+        scanf("%f", &accountNumber);
+        
+        if (accountNumber == -1) break;
+
+        printf("Enter beginning balance: ");
+        scanf("%f", &beginningBalance);
+        printf("Enter total charges: ");
+        scanf("%f", &totalCharges);
+        printf("Enter total credits: ");
+        scanf("%f", &totalCredits);
+        printf("Enter credit limit: ");
+        scanf("%f", &creditLimit);
+
+        balance = beginningBalance + totalCharges - totalCredits;
+        if (balance > creditLimit){
+            printf("Credit limit exceed\n");
+        }else{
+        printf("Account: %f\n", accountNumber);
+        printf("Credit limit: %f\n", creditLimit);
+        printf("Credit balance: %f\n", balance);
+        }
+    }
+
+    return 0;
+}
